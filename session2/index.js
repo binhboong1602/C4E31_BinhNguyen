12198ea46afc30a3300b0b1fdad3b470fb038790
BinhNguyen <anhdoanso1@gmail.com>
2019-05-02 00:58:27 +0700

    baitap

---
+++ session2/index.js
@@ -0,0 +1,51 @@
+  	Answer the questions:
+		1: boolean là một kiểu dữ liệu dùng để mô tả một trong hai giá trị true (đúng) hoặc false (sai)
+		Ví dụ về kiểu boolean :
+		10 > 9 // trả về true
+		10 == 9 // trả về false
+		10 < 9 // trả về false
+
+		2 Lưu đồ thuật toán là công cụ dùng để biểu diễn thuật toán, mô tả nhập (input), 
+					dữ liệu xuất (output) và luồng xữ lý thông qua các ký hiệu hình học
+
+		3 Điều kiện lồng nhau là một điều kiện có thể lồng trong 1 điều kiện khác 
+
+		ví dụ :
+		let x = 10;
+		let y = 10;
+
+		if (x < y){
+		console.log("x là con y");
+		}
+
+		else{
+
+		if (x > y){
+
+		console.log("x là bố y");
+		}
+
+		else{
+
+		console.log("x là anh em song sinh y")
+		}
+
+		}
+
+	
+	Turtle exercises
+		Bài1:
+		 color("red")
+		 for(let i=0; i<4; i++){
+		 lt(30)
+		 fd(100)
+		 rt(60)
+		 fd(100)
+		 rt(120)
+		 fd(100)
+		 rt(60)
+		 fd(100)
+		 lt(30)
+		 rt(90)
+		 };
+		   
